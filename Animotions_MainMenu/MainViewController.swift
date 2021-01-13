e760627d64b7eb56e9108b6c5cde1218193e629a
AaronRachwitz <aaroneaster@u.boisestate.edu>
2021-01-13 13:30:07 -0700

    Pasted in MainMenu Storyboard Folder
    The folder that contained my storyboard files for the main menu so far is what I duplicated into this project, so hopefully it will work ok!

---
+++ Animotions_MainMenu/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Animotions_MainMenu
+//
+//  Created by Aaron Easter on 1/12/21.
+//  Copyright © 2021 Aaron Easter. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToMainViewController(_ segue:UIStoryboardSegue) {
+        //placeholder for unwind segue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Animotions_MainMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animotions_MainMenu
+//
+//  Created by Aaron Easter on 1/12/21.
+//  Copyright © 2021 Aaron Easter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
