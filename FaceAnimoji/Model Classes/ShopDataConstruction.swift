021ba7fe5eaf928cf494de438240ff3d70b6acda
Justin Peters <justin_peters@live.com>
2021-01-26 15:08:06 -0700

    Organized the code more

---
+++ FaceAnimoji/Model Classes/ShopDataConstruction.swift
@@ -0,0 +1,16 @@
+//
+//  ShopDataConstruction.swift
+//  FaceAnimoji
+//
+//  Created by Justin Peters on 1/26/21.
+//  Copyright © 2021 ashutosh.dingankar. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class ShopDataConstruction {
+    
+    
+    
+}
