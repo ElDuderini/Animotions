4e3d23b575d4df9f49fe43a065883fe33273e1dc
Justin Peters <justin_peters@live.com>
2021-02-09 15:17:10 -0700

    Added the ability to have charts in the stats menu

--- FaceAnimoji/View Controllers/StatsViewController.swift
+++ FaceAnimoji/View Controllers/StatsViewController.swift
@@ -110,20 +110,22 @@ class StatsViewController: UIViewController {
     }
     
     func generateChart(chart:LineChartView){
+        
         chart.rightAxis.enabled = false
         chart.backgroundColor = .systemGreen
+        chart.animate(yAxisDuration: 2)
         
         let yAxis = chart.leftAxis
         yAxis.labelFont = .boldSystemFont(ofSize: 15)
-        yAxis.setLabelCount(10, force: false)
+        yAxis.setLabelCount(5, force: false)
         yAxis.labelTextColor = .white
         yAxis.axisLineColor = .white
         yAxis.labelPosition = .outsideChart
         
+        
         let xAxis = chart.xAxis
         xAxis.labelPosition = .bottom
-        xAxis.labelFont = .boldSystemFont(ofSize: 15)
-        xAxis.setLabelCount(10, force: false)
+        xAxis.labelFont = .boldSystemFont(ofSize: 0)
         xAxis.labelTextColor = .white
         
     }
@@ -131,20 +133,102 @@ class StatsViewController: UIViewController {
     
     func setData(chart:LineChartView){
         
-//        var yValues:[ChartDataEntry]
-//        var intex = 0
-//        
-//        if(chart == freeplayChart){
-//            for index freeplayItems.count{
-//                
+        var entries = [ChartDataEntry]()
+        //var referenceTimeInterval : TimeInterval = 0
+        //var dateObjects = [Date]()
+        var index = 1
+        
+
+        if(chart == freeplayChart){
+            
+            for data in freeplayItems{
+                let xAxis = index
+                index += 1
+                
+                let yAxis = Double(data.sucessRate)
+                let entry = ChartDataEntry(x: Double(xAxis), y: yAxis)
+                entries.append(entry)
+            }
+//
+//            if(freeplayItems.count == 0){
+//                return
 //            }
-//        }
-//        else if(chart == lessonChart){
-//            for data in lessonItems{
-//                
+//
+//            for date in freeplayItems{
+//                dateObjects.append(date.sessionDate ?? Date())
 //            }
-//        }
+//
+//            if let minTimeInterval = (dateObjects.map{$0.timeIntervalSince1970}).min() {
+//                referenceTimeInterval = minTimeInterval
+//            }
+//
+//            let formatter = DateFormatter()
+//            formatter.dateStyle = .medium
+//            formatter.timeStyle = .none
+//            formatter.locale = Locale.current
+//
+//            let xValuesNumberFormatter = ChartAxisFormetter(referenceTimeInterval: referenceTimeInterval, dateFormatter: formatter)
+//            freeplayChart.xAxis.valueFormatter = xValuesNumberFormatter
+//
+//            for index in 0...freeplayItems.count - 1{
+//                let timeInterval = dateObjects[index].timeIntervalSince1970
+//                let xValue = (timeInterval - referenceTimeInterval) / (3600 * 24)
+//
+//                let yValue = Double(freeplayItems[index].sucessRate)
+//                let entry = ChartDataEntry(x: xValue, y: yValue)
+//                print(index)
+//                entries.append(entry)
+//            }
+//
+            let dataSet = LineChartDataSet(entries: entries, label: "Sucess rate %")
+            dataSet.mode = .linear
+            dataSet.drawCirclesEnabled = false
+            dataSet.lineWidth = 2.8
+            dataSet.drawFilledEnabled = true
+            dataSet.drawValuesEnabled = false
+            
+            
+
+            let data = LineChartData(dataSet: dataSet)
+
+            freeplayChart.data = data
+            
+            let percentFormat = NumberFormatter()
+            percentFormat.numberStyle = .percent
+            percentFormat.percentSymbol = "%"
+            
+            freeplayChart.xAxis.valueFormatter = DefaultAxisValueFormatter(formatter: percentFormat )
+            
+            
+
+        }
+        else if(chart == lessonChart){
+            
+            for data in lessonItems{
+                let xAxis = index
+                index += 1
+                
+                let yAxis = Double(data.avgTimeForResponse)
+                let entry = ChartDataEntry(x: Double(xAxis), y: yAxis)
+                entries.append(entry)
+            }
+            
+            let dataSet = LineChartDataSet(entries: entries, label: "Average response time in seconds")
+            dataSet.mode = .linear
+            dataSet.drawCirclesEnabled = false
+            dataSet.lineWidth = 2.8
+            dataSet.drawFilledEnabled = true
+            dataSet.drawValuesEnabled = false
+            
+            
+
+            let data = LineChartData(dataSet: dataSet)
+
+            lessonChart.data = data
+        }
     }
+    
+    
     
     @IBAction func backBtn(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
